5fbc9474968ff2e11468e454976dcda03ce2724a
TazwoarCommits <tazwoarlives@gmail.com>
2024-12-29 19:00:46 +0600

    done

--- index.js
+++ index.js
@@ -9,11 +9,6 @@ const port = process.env.PORT || 5000
 app.use(express.json())
 app.use(cors())
 
-// Ask-Queries
-// l1Fgc3Lqvzmx2hBY
-
-
-// const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.rvz6g.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -27,6 +22,8 @@ const client = new MongoClient(uri, {
 
 async function run() {
     try {
+
+        
         const queriesCollection = client.db("Ask-me").collection("queries") ;
         const recommendationCollection = client.db("Ask-me").collection("recommendation") ;
 
@@ -148,10 +145,10 @@ async function run() {
         })
 
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
-        // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
-        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+        // await client.connect();
+        // // Send a ping to confirm a successful connection
+        // await client.db("admin").command({ ping: 1 });
+        // console.log("Pinged your deployment. You successfully connected to MongoDB!");
 
         
     } finally {
